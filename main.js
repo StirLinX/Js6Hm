88aa2720bb271db5447eb4d75d253b7494416908
StirLinX <bolusovnursultan@gmail.com>
2025-08-31 21:36:14 +0500

    Update main.js

--- main.js
+++ main.js
@@ -3,25 +3,23 @@ const items = [];
 while (true) {
     const command = prompt("Введите команду (add имя, del имя, stop):");
 
-    if (command === null || command === "stop") {
+    if(command === null || command === "stop") {
         console.log("Окончательный массив:", items);
         break;
     }
-
     const parts = command.split(" ");
     const action = parts[0];
     const value = parts[1];
-    if (action === "add") {
+    if(action === "add") {
         items.push(value);
         console.log("Добавлен элемент: " + value);
-    } else if (action === "del") {
+    }else if(action === "del") {
         const index = items.indexOf(value);
-        if (index !== -1) {
+        if(index !== -1) {
             items.splice(index, 1);
             console.log("Удален элемент: " + value);
         }
     }
-
     console.log("Текущий массив:", items);
 }
 
@@ -32,7 +30,6 @@ while (true) {
 
 
 const array = [1,2,3,4,5,6,7,8,9,10,11];
-
 const chet = [];
 const nechet = [];
 
